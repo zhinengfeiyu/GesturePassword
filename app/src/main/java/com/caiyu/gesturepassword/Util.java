bea8e82740ce555730b6f12548e302930bb289b3
zhinengfeiyu <yucaidongzhou@gmail.com>
2018-01-23 15:34:39 +0800

    冲突测试

--- app/src/main/java/com/caiyu/gesturepassword/Util.java
+++ app/src/main/java/com/caiyu/gesturepassword/Util.java
@@ -10,33 +10,24 @@ import android.content.Context;
  */
 public class Util {
 
-
+    //dp转px
     public static int dp2px(Context context, float dpValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
+        final float scale=context.getResources().getDisplayMetrics().density;
         return (int) (dpValue * scale + 0.5f);
     }
 
     public static int sp2px(Context context, float spValue) {
         final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        
         return (int) (spValue * fontScale + 0.5f);
     }
 
     public static double change(double a){
         return a * Math.PI  / 180;
     }
-
-
-
+    
     public static double changeAngle(double a){
         return a * 180 / Math.PI;
-    }
-
-    public void test() {}
-
-    public void t12345() {
-        int a = 1;
-        int b = 2;
-        return;
     }
 
 }
